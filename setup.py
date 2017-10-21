48e60b57b441a73389a280a6b06439183aedacdf
Sebastian Rittau <srittau@rittau.biz>
2017-10-21 18:20:44 +0200

    add more meta data to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
 
+import os
+
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name="rouver",
     version="0.1.0",
     description="A microframework",
+    long_description=read("README.rst"),
     author="Sebastian Rittau",
     author_email="srittau@rittau.biz",
     url="https://github.com/srittau/rouver",
     packages=["rouver"],
     install_requires=["werkzeug >= 0.12.0"],
     tests_require=["asserts"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
 )
