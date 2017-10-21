f2c2058570bf5c36b7ab04514b792cd980e68b17
Sebastian Rittau <srittau@rittau.biz>
2017-10-21 11:25:40 +0200

    [api] add HeaderType, StartResponseReturnType, and StartResponseType

--- rouver/types.py
+++ rouver/types.py
@@ -1,4 +1,12 @@
-from typing import Dict, Any
+from typing import Callable, Tuple, List, Dict, Any
 
+# (name, value)
+HeaderType = Tuple[str, str]
 
 EnvironmentType = Dict[str, Any]
+
+# (body) -> None
+StartResponseReturnType = Callable[[bytes], None]
+
+# (status, headers) -> write
+StartResponseType = Callable[[str, List[HeaderType]], StartResponseReturnType]
