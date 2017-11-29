3c183a6f01b99bea4dc92c85e02f80e3302da1dd
Sebastian Rittau <srittau@rittau.biz>
2017-11-29 17:55:41 +0100

    Bump to version 0.4.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="rouver",
-    version="0.4.4",
+    version="0.4.5",
     description="A microframework",
     long_description=read("README.rst"),
     author="Sebastian Rittau",
