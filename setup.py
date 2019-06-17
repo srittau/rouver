679f3db2c4e26f50002c1e71199436f0de8e9984
Sebastian Rittau <srittau@rittau.biz>
2019-06-17 20:31:06 +0200

    Update development status to beta

--- setup.py
+++ setup.py
@@ -19,19 +19,17 @@ setup(
     url="https://github.com/srittau/rouver",
     packages=["rouver", "rouver_test"],
     package_data={"rouver": ["py.typed"]},
-    install_requires=[
-        "dectest >= 1.0.0, < 2",
-        "werkzeug >= 0.12.0",
-    ],
+    install_requires=["dectest >= 1.0.0, < 2", "werkzeug >= 0.12.0"],
     tests_require=["asserts >= 0.8.5, < 0.9"],
     python_requires=">= 3.5",
     license="MIT",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
-    ])
+    ],
+)
