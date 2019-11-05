cc5654360aba669cbc1eba4b2082e26d7cf70411
Sebastian Rittau <srittau@rittau.biz>
2019-11-05 16:04:45 +0100

    Set version number to 1.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="rouver",
-    version="0.99.2",
+    version="1.0.0",
     description="A microframework",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
