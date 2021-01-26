204e8b624a0a19921b0d5fffd28d0a0663a122b4
Sebastian Rittau <srittau@rittau.biz>
2021-01-26 19:34:23 +0100

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="rouver",
-    version="2.2.0",
+    version="2.2.1",
     description="A microframework",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
